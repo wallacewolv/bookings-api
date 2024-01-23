efb0d6c8ba5caa33888a18a6e981eadf155eb852
wallace <wallace_wolv@hotmail.com>
2024-01-22 23:10:47 -0300

    feat: Criando metodo login no AuthService

--- src/auth/AuthService.js
+++ src/auth/AuthService.js
@@ -18,8 +18,16 @@ class AuthService {
     return newUser;
   }
 
-  login() {
+  login(email, password) {
+    const user = this.repository.findByEmail(email);
+    if (!user) {
+      throw new Error("User not found.");
+    }
 
+    const isSamePassword = bcrypt.compareSync(password, user.password);
+    if (!isSamePassword) {
+      throw new Error("Wrong password.");
+    }
   }
 }
 
