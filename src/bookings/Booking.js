8f49b6982ff5a8448549609a9c9a5be02a2025a0
wallace <wallace_wolv@hotmail.com>
2024-01-23 00:13:52 -0300

    feat: Adicionando user na criação da reserva

--- src/bookings/Booking.js
+++ src/bookings/Booking.js
@@ -3,8 +3,9 @@ const { v4: uuidv4 } = require('uuid');
 // id, roomId, guestName, checkInDate, checkOutDate
 
 class Booking {
-  constructor(roomId, guestName, checkInDate, checkOutDate) {
-    this.id = uuidv4();
+  constructor({ id, user, roomId, guestName, checkInDate, checkOutDate }) {
+    this.id = id ?? uuidv4();
+    this.user = user;
     this.roomId = roomId;
     this.guestName = guestName;
     this.checkInDate = checkInDate;
