128b37cde9e4a4d6437def57ca477cfb2bc328f1
wallace <wallace_wolv@hotmail.com>
2024-01-22 23:29:48 -0300

    feat: Criando endpoint de register no app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const fastify = require('fastify');
+
 const BookingRepository = require('./bookings/BookingRepository');
 const BookingService = require('./bookings/BookingService');
 const BookingController = require('./bookings/BookingController');
+
+const UserRepository = require('./auth/UserRepository');
+const AuthService = require('./auth/AuthService');
+const AuthController = require('./auth/AuthController');
 
 const app = fastify({ logger: true });
 
 const bookingRepository = new BookingRepository();
 const bookingService = new BookingService(bookingRepository);
 const bookingController = new BookingController(bookingService);
+
+const userRepository = new UserRepository();
+const authService = new AuthService(userRepository);
+const authController = new AuthController(authService);
 
 app.get("/hello", (request, reply) => {
   reply.send({ message: "Hello, World!" });
@@ -21,6 +30,11 @@ app.get("/api/bookings", (request, reply) => {
 app.post("/api/bookings", (request, reply) => {
   const { code, body } = bookingController.save(request);
   reply.code(code).send({ body });
+});
+
+app.post("/api/auth/register", (request, reply) => {
+  const { code, body } = authController.register(request);
+  reply.code(code).send(body);
 });
 
 module.exports = app;
